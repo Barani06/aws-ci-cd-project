6be8cfacb0e428de7c8a0e584f338e3198bb0113
Barani06 <sbarani275@gmail.com>
2026-04-17 13:57:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Auto deploy test ")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🚀 CI/CD Live App Working!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
